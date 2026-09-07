16306e180d203162391877e2fa4d7a066afca26b
Adefemi Adeoye <adefemiadeoye@yahoo.com>
2026-09-07 09:20:48 +0100

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Igniton started";
+    std::cout << "Ignition started";
     httplib::Server svr;
 
     svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
