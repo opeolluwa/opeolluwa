58126e6fad4333c6aeadf1b7bfa4506b3f3aa5dc
Adefemi Adeoye <adefemiadeoye@yahoo.com>
2026-09-07 09:20:40 +0100

    hello world

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <httplib.h>
+#include <iostream>
+
+int main() {
+    std::cout << "Igniton started";
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("Hello, World!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 8080);
+}
