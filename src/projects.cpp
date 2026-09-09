353d9a97f5a6b82c41f05193664fccc3ee8982bb
Adefemi Adeoye <adefemiadeoye@yahoo.com>
2026-09-09 19:48:18 +0100

    chore

--- src/projects.cpp
+++ src/projects.cpp
@@ -1,1 +1,11 @@
-#include <projects.hpp>
+#include <include/projects.hpp>
+#include <vector>
+
+const std::vector<Project> featured_projects = {
+    Project{
+      name : "Lunar",
+      description : "Some description",
+      repository_url : "https://github.com/opeolluwa/lunar",
+      technologies : {"Nuxt", "Pinia", "Rust", "Android", "Tauri"}
+    },
+};
